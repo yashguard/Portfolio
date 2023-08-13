11e409839b6bef1f1e1af69e6ef8b90c8e78f198
Yash Guard <yashguard>
2023-08-13 18:11:47 +0530

    Framer motion almost done

--- src/Components/ProjectDemo.jsx
+++ src/Components/ProjectDemo.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
-const ProjectDemo = () => {
+const ProjectDemo = (props) => {
+  let { bgColor, option } = props;
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <motion.div
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="box" style={{ backgroundColor: bgColor }}>
+        <div className="overlay-box row align-items-center justify-content-center">
+          <h2>Title</h2>
+          <span>JQUERY, HTML, CSS, MEDIAQUERY</span>
+        </div>
+        {/* <img src="/image/lawsight.png" alt="" /> */}
+      </div>
+    </motion.div>
+  );
+};
 
-export default ProjectDemo
+export default ProjectDemo;
--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -4,34 +4,113 @@ import ProjectButton from "../Components/ProjectButton";
 import ProjectDemo from "../Components/ProjectDemo";
 import { useDispatch, useSelector } from "react-redux";
 import { addProjects } from "../Redux/Action";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Portfolio = () => {
   let [activeLink, setActiveLink] = useState("ALL");
   let projectDetails = [
     {
+      id: 1,
+      title: "",
+      image: "",
+      languages: "abcd",
       option: "CSS",
+      bgColor: "red",
     },
     {
-      option: "CSS",
-    },
-    {
-      option: "SASS",
-    },
-    {
-      option: "SASS",
-    },
-    {
-      option: "BOOTSTRAP",
-    },
-    {
+      id: 2,
+      title: "",
+      image: "",
+      languages: "abcd",
       option: "REACT JS",
+      bgColor: "black",
+    },
+    {
+      id: 3,
+      title: "",
+      image: "",
+      languages: "abcd",
+      option: "SASS",
+      bgColor: "yellow",
+    },
+    {
+      id: 4,
+      title: "",
+      image: "",
+      languages: "abcd",
+      option: "CSS",
+      bgColor: "red",
+    },
+    {
+      id: 5,
+      title: "",
+      image: "",
+      languages: "abcd",
+      option: "BOOTSTRAP",
+      bgColor: "blue",
+    },
+    {
+      id: 6,
+      title: "",
+      image: "",
+      languages: "abcd",
+      option: "SASS",
+      bgColor: "yellow",
+    },
+    {
+      id: 7,
+      title: "",
+      image: "",
+      languages: "abcd",
+      option: "CSS",
+      bgColor: "red",
+    },
+    {
+      id: 8,
+      title: "",
+      image: "",
+      languages: "abcd",
+      option: "REACT JS",
+      bgColor: "black",
+    },
+    {
+      id: 9,
+      title: "",
+      image: "",
+      languages: "abcd",
+      option: "SASS",
+      bgColor: "yellow",
+    },
+    {
+      id: 10,
+      title: "",
+      image: "",
+      languages: "abcd",
+      option: "CSS",
+      bgColor: "red",
+    },
+    {
+      id: 11,
+      title: "",
+      image: "",
+      languages: "abcd",
+      option: "BOOTSTRAP",
+      bgColor: "blue",
+    },
+    {
+      id: 12,
+      title: "",
+      image: "",
+      languages: "abcd",
+      option: "SASS",
+      bgColor: "yellow",
     },
   ];
   let dispatch = useDispatch();
   let projects = useSelector((store) => store.projects);
   const handleActiveLink = async (value) => {
     await setActiveLink(value);
-    handleProjects(value);
+    await handleProjects(value);
   };
 
   const handleProjects = (value) => {
@@ -51,42 +130,43 @@ const Portfolio = () => {
     <section className="project-section" id="portfolio">
       <div className="container">
         <CommonHeading heading="PORTFOLIO" subHeading="My Work" />
-        <div className="project-buttons row justify-content-between">
-          <ProjectButton
-            option="ALL"
-            handleActiveLink={handleActiveLink}
-            activeLink={activeLink}
-          />
-          <ProjectButton
-            option="CSS"
-            handleActiveLink={handleActiveLink}
-            activeLink={activeLink}
-          />
-          <ProjectButton
-            option="SASS"
-            handleActiveLink={handleActiveLink}
-            activeLink={activeLink}
-          />
-          <ProjectButton
-            option="BOOTSTRAP"
-            handleActiveLink={handleActiveLink}
-            activeLink={activeLink}
-          />
-          <ProjectButton
-            option="REACT JS"
-            handleActiveLink={handleActiveLink}
-            activeLink={activeLink}
-          />
-        </div>
-        <div className="project-demo">
-          {projects.map((ele, i) => {
-            return (
-              <div className="box" key={i}>
-                <h1>{ele.option}</h1>
-              </div>
-            );
-          })}
-          {/* <ProjectDemo /> */}
+        <div className="project-box">
+          <div className="project-buttons row justify-content-between">
+            <ProjectButton
+              option="ALL"
+              handleActiveLink={handleActiveLink}
+              activeLink={activeLink}
+            />
+            <ProjectButton
+              option="CSS"
+              handleActiveLink={handleActiveLink}
+              activeLink={activeLink}
+            />
+            <ProjectButton
+              option="SASS"
+              handleActiveLink={handleActiveLink}
+              activeLink={activeLink}
+            />
+            <ProjectButton
+              option="BOOTSTRAP"
+              handleActiveLink={handleActiveLink}
+              activeLink={activeLink}
+            />
+            <ProjectButton
+              option="REACT JS"
+              handleActiveLink={handleActiveLink}
+              activeLink={activeLink}
+            />
+          </div>
+          <motion.div className="project-demo">
+            {projects.map((ele, i) => {
+              return (
+                <AnimatePresence>
+                  <ProjectDemo key={ele.id} {...ele} />;
+                </AnimatePresence>
+              );
+            })}
+          </motion.div>
         </div>
       </div>
     </section>
